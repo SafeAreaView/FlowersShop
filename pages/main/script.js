a7d2d9239ac4f2e89b29fb2ac2224c84437e42c2
Max Cherniavsky <maxtwain777@gmail.com>
2022-10-13 08:04:13 +0300

    feat: add simple validation form

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,6 +1,6 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
 btn.onclick = function () {
   modal.style.display = "block";
 };
@@ -12,3 +12,33 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+//form validation
+
+let inputs = document.querySelectorAll("input[data-rule]");
+for (let input of inputs) {
+  input.addEventListener("blur", function () {
+    let rule = this.dataset.rule;
+    let value = this.value;
+    let check;
+
+    switch (rule) {
+      case "number":
+        check = /^\d+$/.test(value);
+        break;
+      case "name":
+        check = /^[А-ЯЁ][а-яё]*$/.test(value);
+        break;
+      case "email":
+        check = /.+@.+\..+/i.test(value);
+        break;
+    }
+    if (check) {
+      this.classList.remove("invalid");
+      this.classList.add("valid");
+    } else {
+      this.classList.remove("valid");
+      this.classList.add("invalid");
+    }
+  });
+}
